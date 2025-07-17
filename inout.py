3a115c8cb38e9139946b4a41b07827faa1ed0272
Shreyash07112003 <shreyashlavande2003@gmail.com>
2025-07-17 15:05:00 +0530

    Initial commit

---
+++ inout.py
@@ -0,0 +1,23 @@
+# name = input("Enter your name: ")
+# print("Hello",name,"Welcome")
+
+# print("Hello World")
+
+# s = "Shreyash"
+# print(s)
+
+s = "Shubham"
+age = 25
+city = "Mumbai"
+print(s,age,city)
+
+#x,y = input("Enter the two values")
+#print("Number of boys ",x)
+#print("Number of girls",y)
+
+# colour = input("What is the colour of rose :")
+# print (colour)
+
+#n = int(input("How many roses ?"))
+#print (n)
+#print(type(n))
