82cf8438ee8b16a698f5ca658c7756ea0ad01785
Shreyash07112003 <shreyashlavande2003@gmail.com>
2025-07-17 15:46:21 +0530

    Added datatype.py file

---
+++ datatype.py
@@ -0,0 +1,50 @@
+# #Basic data types
+# s = 20
+# print(s)
+# print(type(s))
+
+# pi = 3.14
+# print(pi)
+# print(type(pi))
+
+# z = 2.3j
+# print(z)
+# print(type(z))
+
+# flag = True
+# print(flag)
+# print(type(flag))
+
+# name = "Shreyash"
+# print(name)
+# print(type(name))
+
+
+#Sequence data types
+    #List
+fruits = ["apple","mango","banana"]
+print(fruits)
+print(type(fruits))
+
+    #Tupple
+point = (10,20,30)
+print(point)
+print(type(point))
+
+    #range
+nums = range(5)
+print(nums)
+print(range(6))
+print(type(nums))
+
+    #set
+s = {1,2,3,4}
+print(s)
+print(type(s))
+
+    #frozenset
+fs = frozenset({1,2,3,4})
+print(fs)
+print(type(fs))
+
+
